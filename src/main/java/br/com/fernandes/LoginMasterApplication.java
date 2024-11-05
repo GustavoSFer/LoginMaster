776557c752a09626bf70447d4285031128a316ee
GustavoSFer <gustavo.sfernander@gmail.com>
2024-11-05 20:56:13 -0300

    Feat: Iniciando o projeto

---
+++ src/main/java/br/com/fernandes/LoginMasterApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginMasterApplication.class, args);
+	}
+
+}

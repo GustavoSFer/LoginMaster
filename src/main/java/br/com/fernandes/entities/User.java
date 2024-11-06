ecc43ea20c114d7156b744cc50da01a2ace2c300
GustavoSFer <gustavo.sfernander@gmail.com>
2024-11-05 21:40:05 -0300

    Feat: Colocando os data notation do spring na entidade user

--- src/main/java/br/com/fernandes/entities/User.java
+++ src/main/java/br/com/fernandes/entities/User.java
@@ -5,11 +5,20 @@ import java.util.Date;
 import java.util.Objects;
 
 import br.com.fernandes.enums.StatusUser;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "Usuarios")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;

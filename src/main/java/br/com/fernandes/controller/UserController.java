18a626ec6006fe3cb9a302c8aa65c0b5cce5aa0d
GustavoSFer <gustavo.sfernander@gmail.com>
2024-11-06 21:19:15 -0300

    Feat: Criando um user pela api

--- src/main/java/br/com/fernandes/controller/UserController.java
+++ src/main/java/br/com/fernandes/controller/UserController.java
@@ -1,13 +1,17 @@
 package br.com.fernandes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fernandes.entities.User;
 import br.com.fernandes.service.UserService;
 
 @RestController
-@RequestMapping(name = "/user")
+@RequestMapping(value = "/user")
 public class UserController {
 	
 	private final UserService userService;
@@ -15,6 +19,13 @@ public class UserController {
 	@Autowired
 	public UserController(UserService userService) {
 		this.userService = userService;
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User create = userService.createUser(user);
+		
+		return ResponseEntity.ok().body(create);
 	}
 
 }
--- src/main/java/br/com/fernandes/service/UserService.java
+++ src/main/java/br/com/fernandes/service/UserService.java
@@ -3,6 +3,7 @@ package br.com.fernandes.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.fernandes.entities.User;
 import br.com.fernandes.repository.UserRepository;
 
 @Service
@@ -13,6 +14,10 @@ public class UserService {
 	@Autowired
 	public UserService(UserRepository userRepository) {
 		this.userRepository = userRepository;		
+	}
+
+	public User createUser(User user) {
+		return userRepository.save(user);
 	}
 
 }
